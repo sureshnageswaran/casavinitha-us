eea33b4b4a50a5e41160fd456872a61b19476dd7
Suresh Nageswaran <sureshnageswaran@yahoo.com>
2023-04-17 14:49:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("update-btn").addEventListener("click", function () {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "<p>The content has been updated!</p>";
+});
